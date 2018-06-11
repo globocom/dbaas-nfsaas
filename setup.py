8fb6855f910e3f84b21b4f0d673df62de00e840c
Elesbom <rafael.goncalves@corp.globo.com>
2018-06-11 15:21:23 -0300

    New version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open('requirements.txt') as requirements_file:
 
 setup(
     name='dbaas_nfsaas',
-    version='0.7.8',
+    version='0.7.9',
     description='NFSaaS integration for DBaaS',
     long_description=readme,
     author='Mauro Andre Murari',
